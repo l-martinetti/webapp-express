14b32504851853cbef2aaec9f9c8c553081f15e9
l-martinetti <luca.martinetti@outlook.it>
2025-02-06 12:55:47 +0100

    feat: import errors handlers to server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const errorsHandler = require('./middlewares/errorsHandler');
+const notFound = require('./middlewares/notFound')
+
 app.use(express.static('public'))
 app.use(express.json())
 
@@ -9,6 +12,8 @@ app.get('/', (req, res) => {
     res.send('Server dei film');
 })
 
+app.use(errorsHandler);
+app.use(notFound);
 
 app.listen(port, (req, res) => {
     console.log(`Sono in ascolto alla porta ${port}`);
