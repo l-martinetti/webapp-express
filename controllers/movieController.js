6761f483748504b731c54d9519cb74d6b20318d4
l-martinetti <luca.martinetti@outlook.it>
2025-02-06 16:08:19 +0100

    feat: add index, show routes
    -Excercise complete

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,13 +1,30 @@
 const connection = require('../data/db')
 
 const index = (req, res) => {
-    res.send('Lista movies')
+    const sql = `SELECT *
+    FROM movies`
+
+    connection.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: 'Query al database fallita' })
+        res.json(results)
+    })
 }
 
 const show = (req, res) => {
     const id = req.params.id
+    const sql = `SELECT *
+    FROM movies M
+    WHERE M.id = ?`
 
-    res.send(`mostra movie con id ${id}`)
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: err })
+        if (results.length === 0 || results[0].id === null) {
+            return res.status(404).json({ error: 'film non trovato' })
+        }
+
+        const movie = results[0]
+        res.json(movie)
+    })
 }
 
 module.exports = {
