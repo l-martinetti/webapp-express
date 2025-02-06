26862cfe37efe7a68923d63ca0b9a4d091ec6db2
l-martinetti <luca.martinetti@outlook.it>
2025-02-06 12:49:42 +0100

    feat: add errors handler middlewares

---
+++ middlewares/notFound.js
@@ -0,0 +1,10 @@
+function notFound (req, res, next){
+    res.status(404);
+    res.json({
+        message: 'Risorsa non trovata',
+        status: 404,
+        error: 'Not Found'
+    })
+}
+
+module.exports = notFound;
