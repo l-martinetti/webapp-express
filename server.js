2e04a44395a23aab6786ce1f21e3fc3cb17746a8
l-martinetti <luca.martinetti@outlook.it>
2025-02-11 18:38:05 +0100

    feat: add cors middleware

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require('cors')
 
 const errorsHandler = require('./middlewares/errorsHandler');
 const notFound = require('./middlewares/notFound')
 const movieRouter = require('./routers/movie');
 const imagePath = require('./middlewares/imagePath')
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 app.use(express.static('public'))
 app.use(express.json())
 app.use(imagePath)
